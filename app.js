1ef422d3b3c2c81a1b3a138b8fc947e8e427e911
Gytis Oz <gytis@oziunas.com>
2017-12-18 22:29:38 +0000

    add databaseurl

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v12");
-mongoose.connect("mongodb://Gytis:akvile@ds161316.mlab.com:61316/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Gytis:akvile@ds161316.mlab.com:61316/yelpcamp");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
