a487bc0d24d01cde536762d93c7ecc871ec1fc29
Gytis Oz <gytis@oziunas.com>
2017-12-18 22:11:05 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+// mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://Gytis:akvile@ds161316.mlab.com:61316/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
